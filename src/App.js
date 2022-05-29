5a6bb40f73c1a624a84d6e74a9011ca76adc6876
Abhay Singh Suryawanshi <choudharyabhay2342@gmail.com>
2022-05-29 20:47:24 +0530

    adding initialState

--- src/App.js
+++ src/App.js
@@ -50,23 +50,25 @@ const particlesInit = async (main) => {
   await loadFull(main);
 };
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+};
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    };
+    this.state = initialState;
   };
 
   loadUser = (data) => {
@@ -158,7 +160,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({ isSignedIn: true });
     }
